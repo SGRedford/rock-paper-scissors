2fab4f918cccc96358b68d70817f1fe9c841cacf
SGRedford <sean.redford@gmail.com>
2023-11-23 01:00:17 -0700

    Completed the if loop for the random guess function

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ if loop to match computer and player choices
 display outcome
 */
 
-let x = Math.floor(Math.random() * 3) + 1;
+let x = Math.floor(Math.random() * 3) + 1; /* This gives me three possible choices 1, 2, or 3 */
 console.log(x);
+
+let randomGuess;
+
+if (x === 1) {
+    randomGuess = "Rock";
+} else if (x === 2) {
+    randomGuess = "Paper";
+} else {
+    randomGuess = "Scissors";
+}
+console.log(randomGuess);
 
