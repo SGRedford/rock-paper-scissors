c1c0e3adbb7c04d45d49cb053a66acfed5b759b4
SGRedford <sean.redford@gmail.com>
2023-11-22 23:27:16 -0700

    Added random number function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 /* Rock Paper Scissors Project */
 
 /* Pseudocode:
-
-
-
+Set up computer guess random number
+direct the number to a guess string
+make var for storing the guess
+set up player choice
+if loop to match computer and player choices
+display outcome
 */
 
-console.log('Hello World');
+let x = Math.floor(Math.random() * 3) + 1;
+console.log(x);
+
